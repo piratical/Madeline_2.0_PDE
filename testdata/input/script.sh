4131a7f3dce2823e6538f6892d84c0aa42181a60
Ed Trager <ed.trager@gmail.com>
2007-02-23 23:30:37 +0000

    (1) Added testdata from the web pages to the SVN repository.
    (2) Moved data to data_old : This needs to eventually
    be removed, but we might want to first check if there's
    anything else left in there we want.
    
    - Ed
    
    
    
    git-svn-id: https://svn.code.sf.net/p/madeline2/code/madeline2/trunk@30 36be61f0-9d1b-0410-b0b4-cea5cbe2d1c6

---
+++ testdata/input/script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mv consanguinity01.data cs_003.data
+mv consanguinity02.data cs_004.data
+mv consanguinity03.data cs_005.data
+mv consanguinity04.data cs_006.data
+mv consanguinity05.data cs_007.data
